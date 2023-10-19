819d95ccfac97cda2e640e7dc2fa2915d43303e7
emilioobertoh <obertoe.1996@gmail.com>
2023-10-19 02:16:21 +0200

    Initial template of a .NET console app to interact with azure blob storage account

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+
+namespace BloblPractice
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Securely set up our connection string
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            var config = builder.Build();
+            var getConnectionString = config["connectionString"];
+        }
+    }
+}
